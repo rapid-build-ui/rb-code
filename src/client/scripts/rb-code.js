b3b23fbb81f1bff312674d29cc564eca6c21fc97
Judd Younce <jyounce@yahoo.com>
2019-05-13 12:54:52 -0500

    feat(styles): add styles and html for titlebar section

--- src/client/scripts/rb-code.js
+++ src/client/scripts/rb-code.js
@@ -3,12 +3,23 @@
  **********/
 import { RbBase, props, html } from '../../rb-base/scripts/rb-base.js';
 import template                from '../views/rb-code.html';
+import '../../rb-popover/scripts/rb-popover.js';
 
 export class RbCode extends RbBase() {
+	viewReady() {
+		super.viewReady && super.viewReady();
+		Object.assign(this.rb.elms, {
+			textarea: this.shadowRoot.querySelector('textarea'),
+		});
+	}
+
 	/* Properties
 	 *************/
 	static get props() {
 		return {
+			caption: Object.assign({}, props.string, {
+				default: 'html'
+			}),
 			kind: Object.assign({}, props.string, {
 				default: 'default'
 			})
